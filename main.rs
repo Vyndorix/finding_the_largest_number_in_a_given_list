1a425513f6366fdb7032a441d36ef69823037bef
Vyndorix <128769129+Vyndorix@users.noreply.github.com>
2023-04-08 22:19:21 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -9,32 +9,32 @@ fn main(){
 
     loop{
         let mut number_list = vec![];
-    for i in 0..13{
-        let mut random = ChaCha8Rng::from_entropy();
-        let random1 = random.gen_range(0..=1000000);
-        number_list.push(random1);
-    }
-
-    println!("Before: ");
-
-
-    let mut largest = &number_list[0];
-
-    for _i in &number_list{
-        println!("{_i}");
-    }
-
-    for number in &number_list{
-        if number > largest{
-            largest = number;
-
+        for i in 0..13{
+            let mut random = ChaCha8Rng::from_entropy();
+            let random1 = random.gen_range(0..=1000000);
+            number_list.push(random1);
         }
-    }
-    thread::sleep(Duration::from_secs(3));
 
-    println!("\n\n\nthe highest number within that vector is: {largest}");
+        println!("Before: ");
 
-    thread::sleep(Duration::from_secs(1));
+
+        let mut largest = &number_list[0];
+
+        for _i in &number_list{
+            println!("{_i}");
+        }
+
+        for number in &number_list{
+            if number > largest{
+                largest = number;
+
+            }
+        }
+        thread::sleep(Duration::from_secs(3));
+
+        println!("\n\n\nthe highest number within that vector is: {largest}");
+
+        thread::sleep(Duration::from_secs(1));
     }
 
 }
